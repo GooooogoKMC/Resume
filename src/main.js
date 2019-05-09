fa74dca4e5510718148a5c8a195625a0e22ce40d
GoogoKMChi <GoogoKMChi@gmail.com>
2019-05-09 23:50:29 +0800

    删除自动生成的router.js 和 store.js，并新建目录结构并重建连个文件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './conf/router';
+import store from './conf/store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
