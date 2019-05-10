031a68594e7d9204fd4d0fa9eb1e631418689de7
GoogoKMChi <GoogoKMChi@gmail.com>
2019-05-10 22:32:13 +0800

    首页封面以及多种效果绘制完成

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,171 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!--<div id="nav">-->
+    <!--<router-link to="/">Home</router-link>-->
+    <!--|-->
+    <!--<router-link to="/about">About</router-link>-->
+    <!--</div>-->
+    <div id="content">
+      <transition name="fade">
+        <router-view/>
+      </transition>
     </div>
-    <router-view/>
+    <div id="zoom">
+      <div class="iconfont" @click="zoomIn">&#xe98c;</div>
+      <div class="iconfont" @click="zoomOut">&#xe987;</div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      base: 1920,
+      zoomRatio: 1,
+      resizeEvt: 'orientationchange' in window ? 'orientationchange' : 'resize',
+      Html: document.getElementsByTagName('html')[0],
+    };
+  },
+  computed: {
+    baseCompute() {
+      return this.base * this.zoomRatio;
+    },
+  },
+  watch: {
+    zoomRatio() {
+      this.changePage();
+    },
+  },
+  methods: {
+    zoomIn() {
+      if (this.zoomRatio > 0.6) {
+        this.zoomRatio -= 0.05;
+      }
+    },
+    zoomOut() {
+      if (this.zoomRatio < 1.5) {
+        this.zoomRatio += 0.05;
+      }
+    },
+    getRatio() {
+      const d = document;
+      const p = Number(((d.body && d.body.clientWidth) || this.Html.offsetWidth) / this.baseCompute)
+        .toFixed(3);
+      console.log(p);
+      if (p < 1.067) {
+        if (p > 0.5) {
+          return p;
+        }
+        return 0.5;
+      }
+      return 1.067;
+    },
+    changePage() {
+      this.Html.style.fontSize = `${this.getRatio() * 100}px`;
+    },
+  },
+  created() {
+    this.changePage();
+    if (!document.addEventListener) return;
+    window.addEventListener(this.resizeEvt, this.changePage, false);
+    document.addEventListener('DOMContentLoaded', this.changePage, false);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.Html.style.transition = 'all .3s linear';
+    }, 300);
+  },
+  destroyed() {
+    if (!document.addEventListener) return;
+    window.removeEventListener(this.resizeEvt, this.changePage);
+    document.removeEventListener('DOMContentLoaded', this.changePage);
+  },
+};
+</script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #333;
+
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-repeat: repeat;
+    background-image: linear-gradient(90deg, rgba(0, 99, 103, 0.76), rgba(66, 142, 146, 0.76),
+      rgba(0, 54, 58, 0.76), rgba(1, 87, 155, 0.76), rgba(49, 27, 146, 0.76),
+      rgba(136, 14, 79, 0.76), rgba(201, 122, 26, 0.75), rgba(194, 53, 45, 0.75),
+      rgba(63, 81, 181, 0.75), rgba(0, 96, 100, 0.76));
+    /*background-image: linear-gradient(90deg, #006064, #428e92,*/
+    /*#00363a, #01579b, #311b92, #880e4f,*/
+    /*#c97a1a, #c2352d, #3f51b5, #006064);*/
+    background-attachment: fixed;
+    background-size: 5000% 5000%;
+    animation: backgroundColorChange 2400s normal infinite linear;
+    @keyframes backgroundColorChange {
+      from {
+        background-position: 5000% 5000%;
+      }
+      to {
+        background-position: 0 0;
+      }
+    }
+
+    #content {
+      position: relative;
+      min-width: 960px;
+
+      .fade-enter-active,
+      .fade-leave-active {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        transition: all .7s ease-in-out;
+      }
+
+      .fade-enter {
+        transform: translateX(5%);
+        opacity: 0;
+      }
+
+      .fade-leave-to {
+        transform: translateX(-5%);
+      }
+
+      .fade-leave-active {
+        opacity: 0;
+      }
+    }
+
+    #zoom{
+      position: fixed;
+      right: 20px;
+      bottom: 30px;
+      .iconfont{
+        $size: 50;
+        background: #fff;
+        width: $size + px;
+        height: $size + px;
+        margin: 20px 0;
+        border-radius: 50%;
+        box-shadow: 0 0 10px 0 rgba(0,0,0,.3);
+        line-height: $size + px;
+        text-align: center;
+        color: #666;
+        font-size: $size / 2 + px;
+        cursor: pointer;
+        transition: all .3s ease;
+        user-select: none;
+        &:hover{
+          transform: scale(1.03);
+        }
+      }
     }
   }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,202 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="resume-home">
+      <p class="title animated">RESUME</p>
+      <div class="self">
+        <div class="autodyne">
+          <img src="../../public/img/head.png" alt="">
+        </div>
+        <div class="info">
+          <p class="name">刘志达</p>
+          <p class="sub-name">GoogoKMChi</p>
+          <p class="intention">求职意向：Web前端开发</p>
+        </div>
+        <router-link tag="div" :to="{name:'about'}" class="next-page"/>
+      </div>
+      <div class="contact">
+        <div class="iconfont" title="13702003650">&#xe848;13702003650</div>
+        <div class="iconfont" title="GoogoKMChi@gmail.com">&#xe832;GoogoKMChi@gmail.com</div>
+        <div class="iconfont" title="成都市温江区">&#xe7a5;成都市温江区</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+
+
+
+
+
+
+
+<style lang="scss" scoped>
+  $pageThemeColor: #567180;
+  $autodyneSize: 3;
+  .resume-home {
+    width: 7.94rem;
+    height: 11.23rem;
+    background: #fff;
+    margin: .3rem auto;
+    border-radius: 3px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, .7), 0 0 30px -5px rgba(0, 0, 0, .7);
+    border-top: 1px solid transparent;
+
+    .title {
+      margin: 1rem auto 2.5rem;
+      font-size: .6rem;
+      font-weight: bold;
+      color: #A8A8A8;
+      text-align: center;
+      font-family: "Andale Mono";
+      background: url('../assets/text-bg.png') no-repeat;
+      //noinspection CssInvalidPropertyValue
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+      animation: moveBg 90s linear infinite;
+      -webkit-animation: moveBg 90s linear infinite;
+      @keyframes moveBg {
+        0% {
+          background-position: 0% 30%;
+        }
+
+        100% {
+          background-position: 100% 50%;
+        }
+      }
+    }
+
+    .self {
+      position: relative;
+      border-top: 1px solid transparent;
+      border-bottom: 1px solid transparent;
+      background: $pageThemeColor;
+      text-align: center;
+
+      .autodyne {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: $autodyneSize + rem;
+        height: $autodyneSize + rem;
+        border: .04rem solid $pageThemeColor;
+        padding: .2rem;
+        border-radius: 50%;
+        box-sizing: border-box;
+        background: #fff;
+
+        img {
+          display: block;
+          width: 100%;
+          height: 100%;
+          background: $pageThemeColor;
+          border-radius: 50%;
+          transition: all .3s linear;
+          cursor: pointer;
+
+          &:hover {
+            transform: scale(1.01);
+          }
+        }
+      }
+
+      .info {
+        margin-top: $autodyneSize/2 + .3 +rem;
+
+        p {
+          margin: .1rem;
+        }
+
+        .name {
+          font-size: .5rem;
+          font-weight: bold;
+          color: #fff;
+        }
+
+        .sub-name {
+          font-size: .2rem;
+          color: #eee;
+        }
+
+        .intention {
+          margin-bottom: .3rem;
+          font-size: .3rem;
+          color: #333;
+        }
+      }
+
+      .next-page {
+        $nextPageSize: .3;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        width: $nextPageSize + rem;
+        height: $nextPageSize + rem;
+        background: #fff;
+        border-radius: 50%;
+        cursor: pointer;
+        transform: translate(50%, -50%);
+        transition: all .3s ease-in-out;
+
+        &:hover {
+          transform: translate(50%, -50%) scale(1.3);
+        }
+
+        &:before,
+        &:after {
+          content: '';
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          background: #fff;
+          border-radius: 50%;
+          transform: translate(-50%, -50%);
+          animation: wave 1s infinite ease-in-out;
+        }
+
+        &:after {
+          animation-delay: .3s;
+        }
+
+        @keyframes wave {
+          from {
+            width: $nextPageSize + rem;
+            height: $nextPageSize + rem;
+            opacity: 1;
+          }
+          to {
+            width: $nextPageSize * 2 + rem;
+            height: $nextPageSize * 2 + rem;
+            opacity: 0;
+          }
+        }
+      }
+    }
+
+    .contact {
+      margin-top: 2rem;
+      font-size: .18rem;
+      color: $pageThemeColor;
+
+      .iconfont {
+        width: 33.3%;
+        float: left;
+        text-align: center;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+    }
+  }
+</style>
